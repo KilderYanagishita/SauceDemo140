49e4d453b734424dc3ff1793b5aa7524893cecdc
KilderYanagishita <Kilderpro@gmail.com>
2024-07-05 12:03:07 -0300

    Scripts Iniciais feitos com Selenium IDE e WebDriver.

---
+++ test_selecionar_produto_swd.py
@@ -0,0 +1,22 @@
+# 1 - Bibliotecas
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 2 - Classe (Opcional)
+class Teste_Produtos():
+    # 2.1 Atributos
+    url = "https://www.saucedemo.com"
+    # 2.2 Funçoes e Metodos
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+
+    def teardown_method(self, method):
+           self.driver.quit()
+
+
+    def test_selecionar_produto(self):
+         self.driver.get(self.url)
+         self.driver.find_element(By.ID,"user-name").send_keys("standard_user")
+         self.driver.find_element(By.NAME,"password").send_keys("secret_sauce")
